e1df6646f6cff284079095790163fba1d1015fc9
Roc Ho <hepeng@gznb.com>
2017-02-15 10:20:50 +0800

    修复metadata获取的代码

--- meta-annotations.js
+++ meta-annotations.js
@@ -19,6 +19,12 @@ function getType(type) {
     }); });
 }
 function createMetaHandler(handlers) {
+    if (handlers === void 0) { handlers = {
+        handleMethod: function () { },
+        handleType: function () { },
+        handleProp: function () { },
+        handleParam: function () { }
+    }; }
     return function (target, propertyKey, index) {
         if (typeof target === 'function') {
             var typeD = getType(target);
@@ -28,13 +34,15 @@ function createMetaHandler(handlers) {
             var type = target.constructor;
             var typeD = getType(type);
             var getMethod = function (d, name) {
-                return getOrCreate(d.methods, name, function () {
+                var method = getOrCreate(d.methods, name, function () {
                     return {
                         name: name,
                         params: [],
                         returnType: Reflect.getMetadata("design:returntype", target, propertyKey)
                     };
                 });
+                method.returnType = Reflect.getMetadata("design:returntype", target, propertyKey);
+                return method;
             };
             //it's a member
             if (index === undefined) {
@@ -50,7 +58,6 @@ function createMetaHandler(handlers) {
                 if (!methodD.params[index]) {
                     methodD.params[index] = {
                         method: methodD,
-                        type: Reflect.getMetadata("design:paramtypes", target, propertyKey)[index]
                     };
                 }
                 handlers.handleParam(methodD.params[index], methodD, typeD);
@@ -68,70 +75,98 @@ function comment(content) {
         handleType: function (type) {
             type.comment = content;
         },
+        handleMethod: function (method) {
+            method.comment = content;
+        },
+        handleProp: function (prop) {
+            prop.comment = content;
+        },
+        handleParam: function (param) {
+            param.comment = content;
+        }
     });
-    // return function(target: any, propertyKey: string , index : number){
-    //     if(typeof target === 'function') // it's a type
-    //     {
-    //         let typeD = getType(target);
-    //         typeD.comment = content;
-    //     }else{
-    //         let type = target.constructor;
-    //         let typeD = getType(type);
-    //
-    //         let getMethod = function(d,name){
-    //             return getOrCreate(d.methods,name,function(){
-    //                 return {
-    //                     params : []
-    //                 }
-    //             })
-    //         };
-    //
-    //         //it's a member
-    //         if(index === undefined){
-    //             // prop
-    //             typeD.props.set(propertyKey,{
-    //                 comment : content,
-    //                 type : Reflect.getMetadata("design:type", target, propertyKey)
-    //             });
-    //         }
-    //         else if(typeof index === 'number'){
-    //             //method param
-    //             var methodD = getMethod(typeD,propertyKey);
-    //             methodD.params[index] = {
-    //                 comment : content,
-    //                 type : Reflect.getMetadata("design:paramtypes", target, propertyKey)[index]
-    //             };
-    //         }
-    //         else{
-    //             // method
-    //             var methodD = getMethod(typeD,propertyKey);
-    //             methodD.comment = content;
-    //             methodD.returnType = Reflect.getMetadata("design:returntype", target, propertyKey)
-    //         }
-    //     }
-    // };
 }
 exports.comment = comment;
 function router(path, model) {
+    return createMetaHandler({
+        handleType: function (type) {
+            type.router = {
+                path: path,
+                type: model
+            };
+        },
+        handleMethod: function (method) {
+            method.router = {
+                path: path,
+                type: model
+            };
+        }
+    });
 }
 exports.router = router;
 function method(m) {
+    return createMetaHandler({
+        handleMethod: function (method) {
+            method.httpMethod = m;
+        }
+    });
 }
 exports.method = method;
 function post() {
+    return method("POST");
 }
 exports.post = post;
 function get() {
+    return method("GET");
 }
 exports.get = get;
 function query() {
+    return createMetaHandler({
+        handleParam: function (param) {
+            param.source = "query";
+        }
+    });
 }
 exports.query = query;
 function body() {
+    return createMetaHandler({
+        handleParam: function (param) {
+            param.source = "body";
+        }
+    });
 }
 exports.body = body;
 function getMetadatas() {
     return typesData;
 }
 exports.getMetadatas = getMetadatas;
+function outputMarkdown() {
+    getMetadatas().forEach(function (v, k) {
+        console.log();
+        console.log("Type %s", k);
+        console.log("Comment %s", v.comment);
+        console.log();
+        console.log("properties");
+        v.props.forEach(function (v, k) {
+            console.log('name %s', k);
+            console.log('comment %s', v.comment);
+            console.log('type %s', v.type);
+        });
+        console.log();
+        console.log("methods");
+        v.methods.forEach(function (v, k) {
+            console.log("name %s", k);
+            console.log('comment %s', v.comment);
+            console.log("return type %s", v.returnType);
+            v.params.forEach(function (v, i) {
+                console.log('index %d', i);
+                console.log('comment %s', v.comment);
+                console.log('type %s', v.type);
+            });
+            console.log('');
+        });
+        console.log();
+    });
+}
+exports.outputMarkdown = outputMarkdown;
 //# sourceMappingURL=meta-annotations.js.map
--- meta-annotations.ts
+++ meta-annotations.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 interface TypeDefinition{
+    router: {path: String, type: Function};
     comment : string;
     type : Function;
     props : Map<string,PropDefinition>;
@@ -13,6 +14,8 @@ interface PropDefinition{
 }
 
 interface MethodDefinition{
+    httpMethod: String;
+    router: {path: String, type: Function};
     name : string;
     comment : string;
     params : ParamDefinition[]
@@ -20,6 +23,7 @@ interface MethodDefinition{
 }
 
 interface ParamDefinition{
+    source: String;
     index : number;
     comment : string;
     type : Function;
@@ -50,8 +54,14 @@ function createMetaHandler(handlers : {
     handleProp? : (prop : PropDefinition, type : TypeDefinition) => void,
     handleMethod? : ( method : MethodDefinition,type : TypeDefinition) => void,
     handleParam? : (param : ParamDefinition, method : MethodDefinition, type : TypeDefinition) => void
+} = {
+    handleMethod : ()=>{},
+    handleType : ()=>{},
+    handleProp : ()=>{},
+    handleParam : ()=>{}
 }) : any{
     return function(target: any, propertyKey: string , index : number){
+
         if(typeof target === 'function') // it's a type
         {
             let typeD = getType(target);
@@ -61,13 +71,15 @@ function createMetaHandler(handlers : {
             let typeD = getType(type);
 
             let getMethod = function(d,name){
-                return getOrCreate(d.methods,name,function(){
+                let method =  getOrCreate(d.methods,name,function(){
                     return {
                         name,
                         params : [],
                         returnType : Reflect.getMetadata("design:returntype", target, propertyKey)
                     }
-                })
+                });
+                method.returnType = Reflect.getMetadata("design:returntype", target, propertyKey);
+                return method;
             };
 
             //it's a member
@@ -86,7 +98,7 @@ function createMetaHandler(handlers : {
                 if(!methodD.params[index]){
                     methodD.params[index] = {
                         method : methodD,
-                        type : Reflect.getMetadata("design:paramtypes", target, propertyKey)[index]
+                        // type : Reflect.getMetadata("design:paramtypes", target, propertyKey)[index]
                     };
                 }
                 handlers.handleParam(methodD.params[index],methodD,typeD);
@@ -102,79 +114,102 @@ function createMetaHandler(handlers : {
 }
 
 export function comment(content? :string) {
-
     return createMetaHandler({
         handleType : (type)=>{
             type.comment = content;
         },
+        handleMethod : (method)=>{
+            method.comment = content;
+        },
+        handleProp : (prop)=>{
+            prop.comment = content;
+        },
+        handleParam : (param) =>{
+            param.comment = content;
+        }
     });
-    // return function(target: any, propertyKey: string , index : number){
-    //     if(typeof target === 'function') // it's a type
-    //     {
-    //         let typeD = getType(target);
-    //         typeD.comment = content;
-    //     }else{
-    //         let type = target.constructor;
-    //         let typeD = getType(type);
-    //
-    //         let getMethod = function(d,name){
-    //             return getOrCreate(d.methods,name,function(){
-    //                 return {
-    //                     params : []
-    //                 }
-    //             })
-    //         };
-    //
-    //         //it's a member
-    //         if(index === undefined){
-    //             // prop
-    //             typeD.props.set(propertyKey,{
-    //                 comment : content,
-    //                 type : Reflect.getMetadata("design:type", target, propertyKey)
-    //             });
-    //         }
-    //         else if(typeof index === 'number'){
-    //             //method param
-    //             var methodD = getMethod(typeD,propertyKey);
-    //             methodD.params[index] = {
-    //                 comment : content,
-    //                 type : Reflect.getMetadata("design:paramtypes", target, propertyKey)[index]
-    //             };
-    //         }
-    //         else{
-    //             // method
-    //             var methodD = getMethod(typeD,propertyKey);
-    //             methodD.comment = content;
-    //             methodD.returnType = Reflect.getMetadata("design:returntype", target, propertyKey)
-    //         }
-    //     }
-    // };
 }
-
 export function router(path : string, model? : Function){
-
+    return createMetaHandler({
+        handleType : (type) =>{
+            type.router = {
+                path,
+                type :model
+            }
+        },
+        handleMethod : (method) =>{
+            method.router = {
+                path,
+                type : model
+            }
+        }
+    });
 }
 
 export function method(m : string){
-
+        return createMetaHandler({
+        handleMethod : (method) => {
+            method.httpMethod = m;
+        }
+    });
 }
 
 export function post(){
-
+    return method("POST");
 }
 
 export function get(){
-
+    return method("GET");
 }
 
 export function query(){
-
+    return createMetaHandler({
+        handleParam : (param)=>{
+            param.source = "query"
+        }
+    })
 }
 
 export function body(){
-
+    return createMetaHandler({
+        handleParam : (param)=>{
+            param.source = "body"
+        }
+    })
 }
 
 export function getMetadatas(){
     return typesData;
+}
+
+export function outputMarkdown(){
+    getMetadatas().forEach(function(v,k){
+        console.log();
+        console.log("Type %s",k);
+        console.log("Comment %s",v.comment);
+        console.log();
+
+        console.log("properties");
+        v.props.forEach(function(v,k){
+            console.log('name %s',k);
+            console.log('comment %s',v.comment);
+            console.log('type %s',v.type);
+        });
+        console.log();
+
+        console.log("methods");
+        v.methods.forEach(function(v,k){
+            console.log("name %s",k);
+            console.log('comment %s',v.comment);
+            console.log("return type %s",v.returnType);
+            v.params.forEach(function(v,i){
+                console.log('index %d',i);
+                console.log('comment %s',v.comment);
+                console.log('type %s',v.type);
+            });
+            console.log('');
+        });
+
+        console.log();
+    });
 }
